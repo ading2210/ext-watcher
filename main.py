ed6cbbba08f96a55372bd1f8f0e614dea3e5f675
ading2210 <ading2019@gmail.com>
2023-07-13 10:18:44 -0400

    recursively deobfuscate directories

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from modules import updates, crx, deobfuscate
 
-extension_id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"
+extension_id = "iheobagjkfklnlikgihanlhcddjoihkg"
 
 crx_data = updates.download_crx(extension_id)
 crx.extract_crx(crx_data, f"cache/{extension_id}")
 
-with open(f"cache/{extension_id}/js/assets.js") as f:
-  js_string = f.read()
-  deobbed = deobfuscate.process_file(js_string)
-  print(deobbed)
+deobfuscate.process_directory(f"cache/{extension_id}")
--- modules/deobfuscate.py
+++ modules/deobfuscate.py
@@ -4,13 +4,31 @@
 import subprocess
 import shutil
 import io
+import pathlib
 
 webcrack_path = shutil.which("webcrack")
 if not webcrack_path:
   raise FileNotFoundError("Webcrack is not installed, or it is not in your PATH.")
 
-#call webcrack to deobfuscate js file
+#call webcrack to deobfuscate a js file
 def process_file(js_string):
   child = subprocess.Popen([webcrack_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   out = child.communicate(input=js_string.encode())
   return out[0].decode()
+
+#recursively deobfuscate a directory
+#this will overwrite the files that are processed
+def process_directory(dir_path):
+  path = pathlib.Path(dir_path)
+  for item in path.rglob("*"):
+    if not item.is_file(): 
+      continue
+    if not item.suffix == ".js":
+      continue
+    
+    js_string = item.read_text()
+    processed_string = process_file(js_string)
+    item.write_text(processed_string)
+
+    if item.suffixes == [".min", ".js"]:
+      item.rename(item.with_suffix("").with_suffix(".js"))
