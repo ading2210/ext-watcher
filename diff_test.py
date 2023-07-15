43bea5a8c78259377ceaf6f3a42a2c995989b758
ading2210 <ading2019@gmail.com>
2023-07-15 04:27:40 -0400

    generate diffs for entire directories

--- diff_test.py
+++ diff_test.py
@@ -1,3 +1,8 @@
 from modules import compare
+import time
 
-print(compare.compare_directory("cache/haldlgldplgnggkjaafhelgiaglafanh/3.0.6431.1", "cache/haldlgldplgnggkjaafhelgiaglafanh/3.0.6692.1"))
+print("Generating diffs...")
+start = time.time()
+compare_result = compare.compare_directory("cache/haldlgldplgnggkjaafhelgiaglafanh/3.0.6431.1", "cache/haldlgldplgnggkjaafhelgiaglafanh/3.0.6692.1")
+end = time.time()
+print(f"Generating diffs took {round(end-start, 2)} seconds.")
--- modules/compare.py
+++ modules/compare.py
@@ -4,7 +4,13 @@ import difflib
 import filecmp
 import pathlib
 
-suffix_whitelist = [".txt", ".js", ".css", ".html", ".json"]
+def check_utf8(file_path):
+  try:
+    with open(file_path, "r") as f:
+      f.read()
+    return True
+  except UnicodeDecodeError:
+    return False 
 
 def generate_diff(old_text, new_text, old_filename, new_filename):
   old_split = old_text.split("\n")
@@ -15,20 +21,19 @@ def generate_diff(old_text, new_text, old_filename, new_filename):
 
   return diff_text
 
-def generate_diff_from_file(old_file=None, new_file=None):
+def generate_diff_from_file(old_file, new_file):
+  old_text = ""
   if old_file:
     old_text = old_file.read_text()
-  else:
-    old_text = ""
   
+  new_text = ""
   if new_file:
     new_text = new_file.read_text()
-  else:
-    new_text = ""
 
   return generate_diff(old_text, new_text, str(old_file), str(new_file))
 
-def walk_comparison_subdirs(comparison, query, base_path=""):
+#returns list of strings matching the query
+def walk_comparison_subdirs(comparison, query, base_path="", enforce_blacklist=True):
   relative_items = getattr(comparison, query)
   
   absolute_items = []
@@ -52,13 +57,45 @@ def walk_comparison_subdirs(comparison, query, base_path=""):
 
   return absolute_items
 
+def generate_diff_safe(old_file, new_file):
+  if old_file and not old_file.is_file():
+    return generate_diff("[Cannot generate diff: old target is a directory]", "", str(old_file), str(new_file))
+  if new_file and not new_file.is_file():
+    return generate_diff("[Cannot generate diff: new target is a directory]", "", str(old_file), str(new_file))
+  
+  if old_file and not check_utf8(old_file):
+    return generate_diff("[Cannot generate diff: old file does not contain text]", "", str(old_file), str(new_file))
+  if new_file and not check_utf8(new_file):
+    return generate_diff("[Cannot generate diff: new file does not contain text]", "", str(old_file), str(new_file))
+
+  return generate_diff_from_file(old_file, new_file)
+
 def compare_directory(old_dir, new_dir):
   old_dir = pathlib.Path(old_dir)
   new_dir = pathlib.Path(new_dir)
   comparison = filecmp.dircmp(old_dir, new_dir)
 
-  print(walk_comparison_subdirs(comparison, "diff_files"))
-
+  changed_diffs = {}
   for diff_file in walk_comparison_subdirs(comparison, "diff_files"):
-    print(old_dir/diff_file)
-    generate_diff_from_file(old_dir/diff_file, new_dir/diff_file)
+    changed_diffs[diff_file] = generate_diff_safe(old_dir/diff_file, new_dir/diff_file)
+
+  deleted_diffs = {}
+  for deleted_file in walk_comparison_subdirs(comparison, "left_only"):
+    deleted_path = old_dir/deleted_file #is path object, not string
+    deleted_items = [deleted_path]
+    if deleted_path.is_dir():
+      deleted_items += deleted_path.rglob("*")
+    
+    for deleted_item in deleted_items:
+      relative_path_str = deleted_item.relative_to(old_dir)
+      deleted_diffs[relative_path_str] = generate_diff_safe(deleted_item, None)
+  
+  created_diffs = {}
+  for created_file in walk_comparison_subdirs(comparison, "right_only"):
+    created_diffs[created_file] = generate_diff_from_file(None, new_dir/created_file)
+  
+  return {
+    "changed": changed_diffs,
+    "deleted": deleted_diffs,
+    "created": created_diffs
+  }
